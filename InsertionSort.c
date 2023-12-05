ec7888aeae4dd1a7f3afe991aa16e06195a7e918
Divya Dharshan S <97381430+Divyadharshan@users.noreply.github.com>
2023-12-05 21:27:20 +0530

    Create InsertionSort.c

---
+++ InsertionSort.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void print(int arr[], int n){ 
+    for(int i=0; i<n; i++){ 
+        printf("%d ", arr[i]);
+    }
+}
+void insertionSort(int arr[], int n){ 
+    for(int i = 1; i < n; i++){
+        int tmp=arr[i];
+        int j=i-1;
+        while(tmp<arr[j] && j>=0){ 
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=tmp;
+    }
+}
+int main(){
+    int arr[]={99,-5,4,5,2,12};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    insertionSort(arr, n);
+    printf("Elements after sorting:\n");
+    print(arr, n);
+}
